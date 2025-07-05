9363a7dd5c8ef7d83000c8a26d9dca8e150c121b
Bhuvan Kumar <appubhuvancharan1090@gmail.com>
2025-07-05 17:34:00 +0530

    initial commit

---
+++ eleven.js
@@ -0,0 +1,13 @@
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+
+const person1 = new Person("Ram", 23);
+
+
+console.log(person1);          
+console.log(person1.name);     
+console.log(person1.age);      
